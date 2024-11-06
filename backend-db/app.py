dfe58c73e66cfc073e170641ed61d5f8955bbc89
Richard Tjokroutomo <richard.tjokro2@gmail.com>
2024-11-07 01:47:00 +0700

    6th commit completed DB code

--- backend-db/app.py
+++ backend-db/app.py
@@ -1,72 +1,58 @@
 from flask import Flask, request, jsonify
 import os
 
+from test_connection import createEntry, deleteEntry, getEntry, getAllEntry
 
-from processImage import get_corners, crop_image
-from ocr import extract_text
 
 app = Flask(__name__)
 
-# Configure the folder to save uploaded files
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Set the allowed extensions
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET'])
+# 1) insert row to db
+# =============================================================================
+# createEntry(location, origin, destination, status, name, customer_id, id_type, notes, flags)
+@app.route('/insert-db', methods=['POST'])
 def call_server():
+    location = request.args.get('location')
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
+    status = request.args.get('status')
+    name = request.args.get('name')
+    customer_id = request.args.get('customer_id')
+    id_type = request.args.get('id_type')
+    notes = request.args.get('notes')
+    flags = request.args.get('flags')
+
+    usr_id = createEntry(location, origin, destination, status, name, customer_id, id_type, notes, flags)
+    return jsonify({'usr_id': usr_id}), 200
+
+
+# 2) delete row
+# ===========================================================================
+@app.route('/delete-db', methods=['POST'])
+def call_server():
+    rfid = request.args.get('rfid')
+    deleteEntry(rfid)
+    return jsonify({'msg': 'deletion success!'}), 200
+
+
+# 3) get 1 row
+# ===========================================================================
+@app.route('/get-db', methods=['GET'])
+def call_server():
+    rfid = request.args.get('rfid')
+    fetched_row = getEntry(rfid)
+    json_dct = {'rfid': fetched_row[0], 'location': fetched_row[1], 'origin': fetched_row[2], 'destination': fetched_row[3], 'status':fetched_row[4], 'name':fetched_row[5], 'customer_id':fetched_row[6], 'id_type':fetched_row[7], 'notes':fetched_row[8], 'flags':fetched_row[9]}
+    return jsonify(json_dct), 200
+
+
+
+# 4) get all rows
+# ===========================================================================
+@app.route('/get-all-db', methods=['POST'])
+def call_server():
+    param1 = request.args.get('param1')
+    param2 = request.args.get('param2')
     return jsonify({'msg': 'hello!'}), 200
-
-@app.route('/extract-handwritten-labels', methods=['POST'])
-def upload_file():
-    # 1) check if the request has the file part
-    # ============================================================================
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    
-    file = request.files['file']
-
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    # 2) check if the file is allowed
-    # ============================================================================
-    src_filepath = ""
-    if file and allowed_file(file.filename):
-        src_filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(src_filepath)
-        #return jsonify({'message': 'File uploaded successfully', 'file_path': filepath}), 200
-    else:
-        return jsonify({'error': 'File extension not allowed'}), 400
-    
-
-    # 3) process img
-    # ============================================================================
-    src_filename = "modified-" + file.filename 
-    cropped_src_filepath = os.path.join(app.config['UPLOAD_FOLDER'], src_filename)
-    coord_arr = get_corners(src_filepath)
-    crop_image(coord_arr[0][0], coord_arr[0][1], coord_arr[2][0], coord_arr[2][1], src_filepath, cropped_src_filepath)
-    
-
-    # 4) extract text
-    # ============================================================================
-    res_text = extract_text(cropped_src_filepath)
-
-
-    # 5) delete image
-    # ============================================================================
-    os.remove(src_filepath)
-    os.remove(cropped_src_filepath)
-
-    # 6) return extracted text
-    # ============================================================================
-    return jsonify({'extracted_text': res_text}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
